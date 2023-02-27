2d7bd7383d82e043ab907930fec1b5c7e9d2904c
Sergei Sitnikov <sitnikov94@gmail.com>
2023-02-27 07:26:46 +0300

    fixed project add comment use func

--- homework/src/main/Main.java
+++ homework/src/main/Main.java
@@ -2,7 +2,25 @@ import cycle_package.CycleHomework;
 
 public class Main {
     public static void main(String[] args) {
-        CycleHomework.printCycle();
+        createCycle();
+        //CycleHomework.createCycle();
+    }
+
+    public static void createCycle() {
+
+        int oneSizeCycle = 6;
+        int twoSizeCycle = 6;
+        final String PRINT_FIRST_SYMBOL = "*";
+        final String PRINT_SECOND_SYMBOL = "0";
+        final String PRINT_THIRD_SYMBOL = " ";
+
+        CycleHomework CycleHomework = new CycleHomework();
+        System.out.println("PRINT CYCLES:");
+        CycleHomework.FullCycle(oneSizeCycle, twoSizeCycle, PRINT_FIRST_SYMBOL, PRINT_THIRD_SYMBOL);
+        System.out.println();
+        CycleHomework.ContourCycle(oneSizeCycle, twoSizeCycle, PRINT_FIRST_SYMBOL, PRINT_THIRD_SYMBOL, PRINT_SECOND_SYMBOL);
+        System.out.println();
+        CycleHomework.TriangularCycle(oneSizeCycle, twoSizeCycle, PRINT_SECOND_SYMBOL, PRINT_THIRD_SYMBOL, PRINT_SECOND_SYMBOL);
     }
 }
 
--- homework/src/main/cycle_package/CycleHomework.java
+++ homework/src/main/cycle_package/CycleHomework.java
@@ -3,29 +3,31 @@ package cycle_package;
 public class CycleHomework {
 	int firstCounter;
 	int secondCounter;
+	final String FIRST_EXERCISE_TXT = "1. FIRST EXERCISE:";
+	final String SECOND_EXERCISE_TXT = "2. SECOND EXERCISE:";
+	final String THIRD_EXERCISE_TXT = "3. THIRD EXERCISE:";
 
-	public static void printCycle() {
-
-		int oneSizeCycle = 6;
-		int twoSizeCycle = 6;
-		String printFirstSymbol = "*";
-		String printSecondSymbol = "0";
-		String printThirdSymbol = " ";
-
-		CycleHomework CycleHomework = new CycleHomework();
-		System.out.println("PRINT CYCLES:");
-		CycleHomework.FullCycle(oneSizeCycle, twoSizeCycle, printFirstSymbol, printThirdSymbol);
-		System.out.println();
-		CycleHomework.ContourCycle(oneSizeCycle, twoSizeCycle, printFirstSymbol, printThirdSymbol, printSecondSymbol);
-		System.out.println();
-		CycleHomework.TriangularCycle(oneSizeCycle, twoSizeCycle, printFirstSymbol, printThirdSymbol, printSecondSymbol);
-	}
-
-	private void FullCycle(int oneSizeCycle, int twoSizeCycle, String printFirstSymbol, String printThirdSymbol) {
-		System.out.println("1. FIRST EXERCISE:");
+	//	public static void createCycle() {
+//
+//		int oneSizeCycle = 6;
+//		int twoSizeCycle = 6;
+//		String printFirstSymbol = "*";
+//		String printSecondSymbol = "0";
+//		String printThirdSymbol = " ";
+//
+//		CycleHomework CycleHomework = new CycleHomework();
+//		System.out.println("PRINT CYCLES:");
+//		CycleHomework.FullCycle(oneSizeCycle, twoSizeCycle, printFirstSymbol, printThirdSymbol);
+//		System.out.println();
+//		CycleHomework.ContourCycle(oneSizeCycle, twoSizeCycle, printFirstSymbol, printThirdSymbol, printSecondSymbol);
+//		System.out.println();
+//		CycleHomework.TriangularCycle(oneSizeCycle, twoSizeCycle, printFirstSymbol, printThirdSymbol, printSecondSymbol);
+//	}
+	public void FullCycle(int oneSizeCycle, int twoSizeCycle, String printFirstSymbol, String printThirdSymbol) {
+		System.out.println(FIRST_EXERCISE_TXT);
 		//print first cycle
-		for (firstCounter = 0; firstCounter < oneSizeCycle; firstCounter++, System.out.println()) {
-			for (secondCounter = 0; secondCounter < twoSizeCycle; secondCounter++) {
+		for (int firstCounter = 0; firstCounter < oneSizeCycle; firstCounter++, System.out.println()) {
+			for (int secondCounter = 0; secondCounter < twoSizeCycle; secondCounter++) {
 				if (secondCounter != twoSizeCycle - 1) {
 					System.out.print(printFirstSymbol + printThirdSymbol);
 				} else {
@@ -35,35 +37,35 @@ public class CycleHomework {
 		}
 	}
 
-	private void ContourCycle(int oneSizeCycle, int twoSizeCycle, String printStarSymbol, String printSpaceSymbol, String printNullSymbol) {
-		System.out.println("2. SECOND EXERCISE:");
+	public void ContourCycle(int oneSizeCycle, int twoSizeCycle, String printFirstSymbol, String printThirdSymbol, String printSecondSymbol) {
+		System.out.println(SECOND_EXERCISE_TXT);
 		//print second cycle
 		for (firstCounter = 0; firstCounter < oneSizeCycle; firstCounter++, System.out.println()) {
 			for (secondCounter = 0; secondCounter < twoSizeCycle; secondCounter++) {
 				if (secondCounter < twoSizeCycle - 1 && (firstCounter == 0 || firstCounter == oneSizeCycle - 1) || secondCounter == 0) {
-					System.out.print(printStarSymbol + printSpaceSymbol);
+					System.out.print(printFirstSymbol + printThirdSymbol);
 				} else if (secondCounter == twoSizeCycle - 1 && (firstCounter == 0 || firstCounter == oneSizeCycle - 1) || firstCounter == oneSizeCycle - 1 || secondCounter == twoSizeCycle - 1) {
-					System.out.print(printStarSymbol); //печатаем один символ в конце строки
+					System.out.print(printFirstSymbol); //печатаем один символ в конце строки
 				} else {
-					System.out.print(printNullSymbol + printSpaceSymbol);
+					System.out.print(printSecondSymbol + printThirdSymbol);
 				}
 			}
 		}
 	}
 
-	private void TriangularCycle(int oneSizeCycle, int twoSizeCycle, String printStarSymbol, String printSpaceSymbol, String printNullSymbol) {
-		System.out.println("3. THIRD EXERCISE:");
+	public void TriangularCycle(int oneSizeCycle, int twoSizeCycle, String printFirstSymbol, String printThirdSymbol, String printSecondSymbol) {
+		System.out.println(THIRD_EXERCISE_TXT);
 		//third first cycle
 		for (firstCounter = 0; firstCounter < oneSizeCycle; firstCounter++, System.out.println()) {
 			for (secondCounter = 0; secondCounter < twoSizeCycle; secondCounter++) {
 				if (firstCounter > secondCounter && secondCounter != twoSizeCycle - 1) {
-					System.out.print(printNullSymbol + printSpaceSymbol);
+					System.out.print(printSecondSymbol + printThirdSymbol);
 				} else if (firstCounter == secondCounter) {
-					System.out.print(printSpaceSymbol + printSpaceSymbol);
+					System.out.print(printThirdSymbol + printThirdSymbol);
 				} else if (firstCounter < oneSizeCycle && secondCounter == twoSizeCycle - 1) {
-					System.out.print(printStarSymbol); //печатаем один символ в конце строки
+					System.out.print(printFirstSymbol); //печатаем один символ в конце строки
 				} else {
-					System.out.print(printStarSymbol + printSpaceSymbol);
+					System.out.print(printFirstSymbol + printThirdSymbol);
 				}
 			}
 		}
