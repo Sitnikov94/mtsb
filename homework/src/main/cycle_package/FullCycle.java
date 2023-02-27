3692dc6d39842e2852a31a5bf7b56a3c3d96c8d6
Sergei Sitnikov <sitnikov94@gmail.com>
2023-02-28 01:46:37 +0300

    delete type firstCounter and secondCounter in FOR

--- homework/src/main/cycle_package/FullCycle.java
+++ homework/src/main/cycle_package/FullCycle.java
@@ -8,8 +8,8 @@ public class FullCycle {
 	//print first cycle
 	public void fullCycle(int oneSizeCycle, int twoSizeCycle, String printFirstSymbol, String printThirdSymbol) {
 
-		for (int firstCounter = 0; firstCounter < oneSizeCycle; firstCounter++, System.out.println()) {
-			for (int secondCounter = 0; secondCounter < twoSizeCycle; secondCounter++) {
+		for (firstCounter = 0; firstCounter < oneSizeCycle; firstCounter++, System.out.println()) {
+			for (secondCounter = 0; secondCounter < twoSizeCycle; secondCounter++) {
 				System.out.print(printFirstSymbol + printThirdSymbol);
 			}
 		}
