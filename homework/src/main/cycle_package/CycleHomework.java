919126ee629cd6a0644c051f89889abc290692e3
Sergei Sitnikov <sitnikov94@gmail.com>
2023-02-28 01:47:28 +0300

    delete type firstCounter and secondCounter in FOR

--- homework/src/main/cycle_package/CycleHomework.java
+++ homework/src/main/cycle_package/CycleHomework.java
@@ -7,8 +7,8 @@ public class CycleHomework {
 	//print first cycle
 	public void fullCycle(int oneSizeCycle, int twoSizeCycle, String printFirstSymbol, String printThirdSymbol) {
 
-		for (int firstCounter = 0; firstCounter < oneSizeCycle; firstCounter++, System.out.println()) {
-			for (int secondCounter = 0; secondCounter < twoSizeCycle; secondCounter++) {
+		for (firstCounter = 0; firstCounter < oneSizeCycle; firstCounter++, System.out.println()) {
+			for (secondCounter = 0; secondCounter < twoSizeCycle; secondCounter++) {
 				System.out.print(printFirstSymbol + printThirdSymbol);
 			}
 		}
