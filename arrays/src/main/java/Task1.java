787b78dd347fa1d22bb3e6a1b766e39804490553
Sergei Sitnikov <sitnikov94@gmail.com>
2023-04-29 15:57:18 +0300

    удалены рудементы

--- arrays/src/main/java/Task1.java
+++
@@ -1,8 +0,0 @@
-public class Task1 {
-	int numberCheck = 6;
-	public void changeArray(int[] arr) {
-		for (int i = 0; arr.length > i; i++) {
-			arr[i] = (arr[i] < numberCheck) ? arr[i] * 2 : arr[i];
-		}
-	}
-}
