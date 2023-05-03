437326768506820ae8b35eb4c8ac251f05b71646
Сергей Ситников <sitnikov94@gmail.com>
2023-05-03 11:01:39 +0300

    коммит в Competitors как один из возможных вариантов

--- interface/src/main/java/interfaceChallenge/competitors/Competitors.java
+++ interface/src/main/java/interfaceChallenge/competitors/Competitors.java
@@ -2,4 +2,6 @@ package interfaceChallenge.competitors;
 
 public interface Competitors extends Running, Jumping {
     String name();
+/*    boolean maxHeightJump(double maxHeightJump);
+    boolean maxDistanceRun(double maxDistanceRun);*/
 }
